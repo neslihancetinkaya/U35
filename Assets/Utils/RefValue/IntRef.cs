6f89248bec69f5be0f4bb914c19bd0b9c469bc6e
Neslihan Çetinkaya <cetinkayaneslihan98@gmail.com>
2023-06-11 16:41:58 +0300

    Project folders and Event&RefValue system added

---
+++ Assets/Utils/RefValue/IntRef.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Utils.RefValue
+{
+    [CreateAssetMenu]
+    public class IntRef : ScriptableObject
+    {
+        public int Value
+        {
+            get => _value;
+            set
+            {
+                _value = value;
+            }
+        }
+
+        private int _value; 
+    }
+}
